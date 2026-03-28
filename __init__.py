80e83e38cc3b66292e1ad2b4e6d3ebc9741cde0e
tanu677 <nigamtanushri9@gmail.com>
2026-03-28 19:04:34 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from firewall.blocklist import block_ip, is_blocked, show_blocklist, clear_blocklist
-from firewall.rules     import apply_rules, evaluate_batch
-from firewall.response  import handle_alert, handle_batch_alerts, show_alert_summary
